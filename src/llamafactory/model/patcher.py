83566c300f9f3283aba3abc079967496d79ea19b
Kingsley <kingsleydodonow@gmail.com>
2026-04-29 19:07:04 +0800

    fix IMA

--- src/llamafactory/model/patcher.py
+++ src/llamafactory/model/patcher.py
@@ -125,7 +125,7 @@ def patch_qwen3_5_forward(model: "PreTrainedModel") -> None:
             hidden_states, _ = self.self_attn(
                 hidden_states=hidden_states,
                 attention_mask=attention_mask,
-                position_ids=position_ids,
+                position_ids=position_ids[0],  # for text_position_ids
                 past_key_values=past_key_values,
                 cache_position=cache_position,
                 position_embeddings=position_embeddings,
